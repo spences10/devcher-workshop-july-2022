1044a26ae986b769e7ad8c9d737fa56074182a1d
spences10 <spences10apps@gmail.com>
2022-07-14 16:56:36 +0100

    refactor: :recycle: add page transition to layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,6 @@
 <script context="module">
+  import Nav from '$lib/components/nav.svelte'
+  import PageTransition from '$lib/components/page-transition.svelte'
   import { client } from '$lib/graphql/graphql-client'
   import { getAllPages } from '$lib/graphql/graphql-queries'
   import { onMount } from 'svelte'
@@ -6,29 +8,32 @@
   import '../app.css'
 
   /** @type {import('@sveltejs/kit').Load} */
-  export const load = async () => {
+  export const load = async ({ url }) => {
     const { pages } = await client.request(getAllPages)
 
     return {
       props: {
         pages,
+        key: url.pathname,
       },
     }
   }
 </script>
 
 <script>
-import Nav from '$lib/components/nav.svelte';
-
   /** @type {any} */
   export let pages
+  /** @type {string} */
+  export let key
 
   onMount(() => {
     themeChange(false)
   })
 </script>
 
-<Nav {pages} />
-<main class="container max-w-3xl mx-auto px-4">
-  <slot />
-</main>
+<PageTransition refresh={key}>
+  <Nav {pages} />
+  <main class="container max-w-3xl mx-auto px-4">
+    <slot />
+  </main>
+</PageTransition>
